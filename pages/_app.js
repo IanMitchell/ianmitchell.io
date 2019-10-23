dda519af5899f3f7f123b0ed0a731b112f2d012d
Ian Mitchell <ian.mitchel1@live.com>
2019-10-23 13:45:23 -0700

    Next 9 Updates

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,6 @@ import React from 'react';
 import '../sass/main.scss';
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return <Component {...pageProps} />;
